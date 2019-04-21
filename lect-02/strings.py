70a11b581a23b17dcbc88f4ca9bcbfaf7ebbb276
Gilberto Barbosa <32587935+jose-gilberto@users.noreply.github.com>
2019-04-21 16:13:53 -0300

    Working with strings in Python

---
+++ lect-02/strings.py
@@ -0,0 +1,23 @@
+# Strings
+
+hi = 'Hello there'  # ' ' single quotes
+greetings = "Hello" # " " double quotes
+
+fo = "this isn't right"
+
+name = "Jose"
+
+# Concatenate
+greet = greetings + " " + name # Hello Jose
+
+# Operations
+
+3 * 'hi'  # Suce
+len(name) # length of a string
+name[0]   # Returns the first element
+name[len(name) - 1] # Returns the last element
+name[1:3] # Slicing a string
+name[:3]
+name[1:]
+name[:] # Copy the entire string
+
