c25663cdc6cffb65d01d5e18b75428d45f249fc6
Gilberto Barbosa <32587935+jose-gilberto@users.noreply.github.com>
2019-04-21 14:32:33 -0300

    Working with branches on Python, even/odd example

---
+++ lect-01/branching/evenOdd.py
@@ -0,0 +1,12 @@
+# Example branching
+
+x = int(input('Enter a integer: '))
+ 
+if x % 2 == 0:
+    print('')
+    print('Even')
+else:
+    print('')
+    print('Odd')
+    
+print('Done with conditional')
