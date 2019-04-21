80770394b9235a2996ea9c73be1ea099cac4e37d
Gilberto Barbosa <32587935+jose-gilberto@users.noreply.github.com>
2019-04-21 16:19:58 -0300

    Using input and output operations

---
+++ lect-02/inputOutput.py
@@ -0,0 +1,16 @@
+# Output
+x = 1
+print(x)
+
+x_str = str(x)
+print("My fav number is", x, ".", "x =", x)
+print('My fav number is ' + x_str + '. ' + 'x = ' + x_str)
+
+# Input
+
+text = input("Type anything...")
+print(text * 5)
+
+num = int(input("Type a number..."))
+print(num * 5)
+
