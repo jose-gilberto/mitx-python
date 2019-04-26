c2c902d47222bf2edfc190e73f6129ff6ca24bcf
Gilberto Barbosa <32587935+jose-gilberto@users.noreply.github.com>
2019-04-26 15:39:37 -0300

    Introduction to functions in Python

---
+++ lect-04/introductionFunctions.py
@@ -0,0 +1,12 @@
+# Introduction to functions
+
+def is_even(i):
+    """
+    Input: i, a positive int
+    Returns True if i is even, otherwise False
+    """
+    print("hi")
+    return i % 2 == 0
+
+is_even(3) # False
+
